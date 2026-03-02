607a734f83a624339e190259021729c5d030671a
eunji-97 <eunji.ko.dev@gmail.com>
2026-03-02 21:28:11 +0900

    6. State와 생명주기

---
+++ src/chapter_06/NotificationList.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Notification from "./Notification";
+
+const reservedNotifications = [
+    {
+        id: 1,
+        message : "1번 메시지입니다."
+    },
+    {
+        id: 2,
+        message : "2번 메시지입니다."
+    },
+    {
+        id: 3,
+        message : "3번 메시지입니다."
+    },
+]
+
+var timer
+
+class NotificationList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { notifications : [] };
+    }
+
+    componentDidMount() {
+    const { notifications } = this.state;
+    timer = setInterval(() => {
+        if (notifications.length < reservedNotifications.length) {
+            const index = notifications.length;
+            notifications.push(reservedNotifications[index]);
+            this.setState({ notifications : notifications });
+        } else {
+            this.setState({ notifications : [] });
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+
+componentWillUnmount() {
+    if (timer) {
+        clearInterval(timer);
+    }
+}
+
+render() {
+    return (
+        <div>
+            {this.state.notifications.map((notification, index) => {
+                return <Notification key={notification.id} id={notification.id} message={notification.message} />
+            })}
+        </div>  
+        );
+    }
+}
+
+export default NotificationList;
